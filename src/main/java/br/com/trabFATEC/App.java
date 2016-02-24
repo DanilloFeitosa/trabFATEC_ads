23c8bdabad9ce56b71c506d46f0bf01812d0dd6c
DanilloFeitosa <danillo.feitosa@fatec.sp.gov.br>
2016-02-24 18:41:39 -0300

    Meu primeiro commit

---
+++ src/main/java/br/com/trabFATEC/App.java
@@ -0,0 +1,13 @@
+package br.com.trabFATEC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
